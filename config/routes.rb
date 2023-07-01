2552d6daa6dd5404640bf9193386c5fdbd1550c8
sambeck87 <sandro_mipb@hotmail.com>
2023-07-01 11:28:43 -0600

    Setup sign-in and sing.out

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/index'
-  get 'users/new'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "main#index"
 end
