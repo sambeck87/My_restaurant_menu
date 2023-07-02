5e33aab13e4f22fe0dd5f3928bd35ad4ec68712a
sambeck87 <sandro_mipb@hotmail.com>
2023-07-02 15:40:00 -0600

    Create MVC to Dish

---
+++ db/migrate/20230702212821_create_dishes.rb
@@ -0,0 +1,12 @@
+class CreateDishes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dishes do |t|
+      t.string :name
+      t.float :price
+      t.string :image
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
