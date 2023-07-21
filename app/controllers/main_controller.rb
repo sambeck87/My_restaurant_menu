a5391baeac771e103a9200a26ece5bc6394574f3
sambeck87 <sandro_mipb@hotmail.com>
2023-07-20 22:22:15 -0600

    Create the methods to change the language

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,4 +5,23 @@ class MainController < ApplicationController
       redirect_to restaurants_path
     end
   end
+
+  def change_spanish
+    set_language_cookie('es')
+    redirect_back(fallback_location: root_path)
+  end
+
+  def change_english
+    set_language_cookie('en')
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_language_cookie(language)
+    cookies[:language] = {
+      value: language,
+      httponly: true,
+    }
+  end
 end
