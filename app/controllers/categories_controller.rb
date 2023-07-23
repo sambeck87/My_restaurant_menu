556d6158a33f54c30bb62b408d24aecef0a60373
sambeck87 <sandro_mipb@hotmail.com>
2023-07-22 22:28:14 -0600

    limit the dishes tha will display in show category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories/1 or /categories/1.json
   def show
+    @dishes = Dish.where(category_id: params[:id])
   end
 
   # GET /categories/new
