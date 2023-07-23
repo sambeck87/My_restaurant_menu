98deb800585d5e63baae9abe2508c37c865a33f8
sambeck87 <sandro_mipb@hotmail.com>
2023-07-22 22:29:00 -0600

    Add fonts for use with tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,21 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+import defaultTheme from 'tailwindcss/defaultTheme'
 
-module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    fontFamily: {
-      nunito: ['Nunito', sans-serif],
-      montserrat: ['Montserrat Alternates', sans-serif],
-      padauk: ['Padauk', sans-serif],
-    },
+export const content = [
+  './public/*.html',
+  './app/helpers/**/*.rb',
+  './app/javascript/**/*.js',
+  './app/views/**/*.{erb,haml,html,slim}'
+]
+export const theme = {
+  fontFamily: {
+    nunito: ['Nunito', 'sans-serif'],
+    montserrat: ['Montserrat Alternates', 'sans-serif'],
+    padauk: ['Padauk', 'sans-serif'],
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
 }
+export const plugins = [
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/aspect-ratio'),
+  require('@tailwindcss/typography'),
+  require('@tailwindcss/container-queries'),
+]
