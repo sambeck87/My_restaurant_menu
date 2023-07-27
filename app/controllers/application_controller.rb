f1d7216c9ace73d6aea6ed5f91239288812d9349
sambeck87 <sandro_mipb@hotmail.com>
2023-07-26 23:28:07 -0600

    Get the browser language and stablish it as a default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :setup_language
 
   def setup_language
+    unless cookies[:language]
+      cookies[:language] = {
+      value: (request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first),
+      httponly: true,}
+    end
     I18n.locale = cookies[:language] || 'en'
   end
   private
